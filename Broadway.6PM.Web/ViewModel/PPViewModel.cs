b767061533e3c53149d837369433c6153e51bcf8
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2021-07-22 06:37:35 +0545

    Layour and Partial page explained

---
+++ Broadway.6PM.Web/ViewModel/PPViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Broadway._6PM.Web.ViewModel
+{
+    public class PPViewModel
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string ButtonText { get; set; }
+    }
+}
